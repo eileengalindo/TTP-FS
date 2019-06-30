9337e9c64178c68cab44b403a2995f0a31b07fe4
Eileen Galindo <galindo.eileen@gmail.com>
2019-06-29 22:10:31 -0400

    updated user model to encrypt password

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const crypto = require('crypto');
 
 const User = db.define('user', {
   firstName: {
@@ -20,7 +21,15 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
-    allowNull: false
+    get() {
+      return () => this.getDataValue('password');
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    get() {
+      return () => this.getDataValue('salt');
+    }
   },
   balance: {
     type: Sequelize.DECIMAL(10, 2),
@@ -32,3 +41,31 @@ const User = db.define('user', {
 });
 
 module.exports = User;
+
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
+
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex');
+};
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
+  }
+};
+
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate(users => {
+  users.forEach(setSaltAndPassword);
+});
